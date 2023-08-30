b1cc766818ed2ffcfed08b43861b001f6e2031fc
intbird <intbird@users.noreply.github.com>
2023-08-30 11:00:22 +0800

    Delete node directory

--- node/socketio/socketio-server.js
+++
@@ -1,8 +0,0 @@
-const server_path = '/socket/socket.io/'
-
-module.exports = function SocketServer(server) {
-  const socketServer = require('socket.io')(server, {path: server_path})
-  socketServer.on('connection', (socket) => {
-    console.log('a user connected');
-  });
-}
